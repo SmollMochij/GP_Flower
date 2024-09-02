7c4c21d8582765ca382eb014ad39ff46176cb3d7
vina <128270926+SmollMochij@users.noreply.github.com>
2024-09-03 02:12:04 +1200

    game framework files

---
+++ game/defs.h
@@ -0,0 +1,12 @@
+// Nate Evans 21144881
+
+#ifndef DEFS_H
+#define DEFS_H
+
+#define WIDTH 1024
+#define HEIGHT 768
+
+#define SPRITE_PATH "sprite/"
+#define SHADER_PATH ""
+
+#endif // DEFS_H
---
+++ game/game.cpp
@@ -0,0 +1,151 @@
+// COMP710 GP Framework 2022
+// This include:
+#include "game.h"
+// Library includes:
+#include "renderer.h"
+#include "logmanager.h"
+#include "sprite.h"
+#include "scenecheckerboards.h"
+#include "sceneballs.h"
+#include "sceneaut.h"
+
+// Static Members:
+Game *Game::sm_pInstance = 0;
+Game &Game::GetInstance()
+{
+	if (sm_pInstance == 0)
+	{
+		sm_pInstance = new Game();
+	}
+	return (*sm_pInstance);
+}
+void Game::DestroyInstance()
+{
+	delete sm_pInstance;
+	sm_pInstance = 0;
+}
+
+Game::Game()
+	: m_pRenderer(0), m_bLooping(true)
+{
+}
+Game::~Game()
+{
+	delete m_pRenderer;
+	m_pRenderer = 0;
+}
+void Game::Quit()
+{
+	m_bLooping = false;
+}
+
+bool Game::Initialise()
+{
+	int bbWidth = 1024;
+	int bbHeight = 720;
+	m_pRenderer = new Renderer();
+	if (!m_pRenderer->Initialise(true, bbWidth, bbHeight))
+	{
+		LogManager::GetInstance().Log("Renderer failed to initialise!");
+		return false;
+	}
+	renderer.SetClearColour(0, 128, 255);
+
+
+	bbWidth = m_pRenderer->GetWidth();
+	bbHeight = m_pRenderer->GetHeight();
+	m_iLastTime = SDL_GetPerformanceCounter();
+
+	// scenes
+	m_iCurrentScene = 0;
+	m_pScenes = new Scene *[10];
+	Scene **pScenes = m_pScenes;
+
+	pScenes[0] = new SceneCheckerboards();
+	pScenes[0]->Initialise(*m_pRenderer);
+
+	pScenes[1] = new SceneBalls();
+	pScenes[1]->Initialise(*m_pRenderer);
+
+	for (int i = 0; i <= 10; i++)
+	{
+
+		m_scenes.push_back(pScenes[i]);
+	}
+
+	return true;
+}
+bool Game::DoGameLoop()
+{
+	const float stepSize = 1.0f / 60.0f;
+
+	// TODO: Process input here!
+	SDL_Event event;
+	while (SDL_PollEvent(&event) != 0)
+	{
+		continue;
+	}
+
+	if (m_bLooping)
+	{
+
+		Uint64 current = SDL_GetPerformanceCounter();
+		float deltaTime = (current - m_iLastTime) / static_cast<float>(SDL_GetPerformanceFrequency());
+
+		m_iLastTime = current;
+
+		m_fExecutionTime += deltaTime;
+
+		Process(deltaTime);
+
+#ifdef USE_LAG
+		m_fLag += deltaTime;
+
+		int innerLag = 0;
+
+		while (m_fLag >= stepSize)
+		{
+			Process(stepSize);
+
+			m_fLag -= stepSize;
+
+			++m_iUpdateCount;
+			++innerLag;
+		}
+#endif // USE_LAG
+
+		Draw(*m_pRenderer);
+	}
+
+	return m_bLooping;
+}
+
+void Game::Process(float deltaTime)
+{
+	ProcessFrameCounting(deltaTime);
+
+	m_scenes[m_iCurrentScene]->Process(deltaTime);
+}
+void Game::Draw(Renderer &renderer)
+{
+	m_iFrameCount++;
+
+	renderer.Clear();
+
+	m_scenes[m_iCurrentScene]->Draw(renderer);
+
+	renderer.Present();
+}
+void Game::ProcessFrameCounting(float deltaTime)
+{
+	// Count total simulation time elapsed:
+	m_fElapsedSeconds += deltaTime;
+
+	// Frame Counter:
+	if (m_fElapsedSeconds > 1.0f)
+	{
+		m_fElapsedSeconds -= 1.0f;
+		m_iFPS = m_iFrameCount;
+		m_iFrameCount = 0;
+	}
+}
